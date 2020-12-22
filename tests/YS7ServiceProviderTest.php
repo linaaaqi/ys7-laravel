f90be7da1a47457bc8174697f0f29e5e9577c8c9
losgif <losgif@gmail.com>
2020-12-22 13:49:21 +0800

    ⚡️更新单元测试

--- tests/YS7ServiceProviderTest.php
+++ tests/YS7ServiceProviderTest.php
@@ -1,16 +1,29 @@
 <?php
 
-namespace Losgif\YS7Laravel;
+namespace Losgif\YS7Laravel\Tests;
 
+use Losgif\YS7Laravel\YS7Auth;
+use Losgif\YS7Laravel\YS7Client;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class YS7ServiceProviderTest
+ *
+ * @package Losgif\YS7Laravel\Tests
+ */
 class YS7ServiceProviderTest extends TestCase
 {
     /**
-     * @return YS7ServiceProvider
+     * @return \Losgif\YS7Laravel\YS7Client
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function testGetYS7Client(): YS7ServiceProvider
+    public function testGetYS7Client(): YS7Client
     {
-        return new YS7ServiceProvider(app());
+        $appKey = config('ys7.app_key');
+        $appSecret = config('ys7.app_secret');
+
+        $auth = new YS7Auth($appKey, $appSecret);
+
+        return new YS7Client($auth);
     }
 }
